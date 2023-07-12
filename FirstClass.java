201a0c6e4a136c5d850d6699b801c3e1fca90f88
DJ Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-07-12 15:38:21 +0100

    Add files via upload

--- FirstClass.java
+++ FirstClass.java
@@ -12,11 +12,24 @@ Should begin with a letter, currency character $, or _
 JAVA MODIFIERS
 Access Modifiers - default, public, protected, private
 Non-access Modifiers - final, abstract, strictfp
+
+JAVA ENUMS
+Name for values in an enumerated list.
+Enums restrict a variable to have one of only a few predefined values.
+
+INHERITANCE (keyword - extends)
+Method to create a hierarchy between classes by inheriting from other classes
+**Classes are able to inherit attributes and methods from other classes using 'extends' keyword
+
+superclass (parent) - class being inherited from
+subclass (child) - class that inherits from another class
+
  */
-public class FirstClass {
 
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
+public class FirstClass {  // class
 
-}
+    public static void main(String[] args){  // method signature
+        System.out.println("Hello World!");  // statement
+    }  // end of method
+
+}  // end of class
