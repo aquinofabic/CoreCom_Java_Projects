02504b377cf76c57f8c58cd95a0144b9a6da4b2c
Dale Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-07-12 15:26:54 +0100

    Update

---
+++ src/FirstClass.java
@@ -0,0 +1,35 @@
+/*
+BASIC SYNTAX
+Java is case sensitive.
+All class names MUST have an UpperCase, including all words within a class.
+All method names SHOULD start with a lowercase.
+Program File Name should exactly match the class name
+
+JAVA IDENTIFIERS
+all components require names, classes, variables, methods,... called identifiers
+Should begin with a letter, currency character $, or _
+
+JAVA MODIFIERS
+Access Modifiers - default, public, protected, private
+Non-access Modifiers - final, abstract, strictfp
+
+JAVA ENUMS
+Name for values in an enumerated list.
+Enums restrict a variable to have one of only a few predefined values.
+
+INHERITANCE (keyword - extends)
+Method to create a hierarchy between classes by inheriting from other classes
+**Classes are able to inherit attributes and methods from other classes using 'extends' keyword
+
+superclass (parent) - class being inherited from
+subclass (child) - class that inherits from another class
+
+ */
+
+public class FirstClass {  // class
+
+    public static void main(String[] args){  // -method signature
+        System.out.println("Hello World!");  // statement
+    }  // end of method
+
+}  // end of class
