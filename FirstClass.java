a1813fb1021925a74ed492689a7c46e2b8b76ae7
DJ Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-07-12 12:31:20 +0100

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,22 @@
+/*
+BASIC SYNTAX
+Java is case sensitive.
+All class names MUST have an UpperCase, including all words within a class.
+All method names SHOULD start with a lowercase.
+Program File Name should exactly match the class name
+
+JAVA IDENTIFIERS
+all components require names, classes, variables, methods,... called identifiers
+Should begin with a letter, currency character $, or _
+
+JAVA MODIFIERS
+Access Modifiers - default, public, protected, private
+Non-access Modifiers - final, abstract, strictfp
+ */
+public class FirstClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
